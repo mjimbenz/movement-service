e326f0092dcb9466c0cd82b7c3a3e394117cad06
mjimbenz <MiguelAutberto.JimenezBenzol@emeal.nttdata.com>
2026-03-25 22:19:50 -0500

    fix: update sonnar issues

--- src/main/java/com/bank/movement/config/KafkaConfig.java
+++ src/main/java/com/bank/movement/config/KafkaConfig.java
@@ -23,7 +23,7 @@ public class KafkaConfig {
 
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
 
         // AVRO
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
--- src/main/java/com/bank/movement/config/KafkaConsumerConfig.java
+++ src/main/java/com/bank/movement/config/KafkaConsumerConfig.java
@@ -3,6 +3,7 @@ package com.bank.movement.config;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -20,7 +21,8 @@ public class KafkaConsumerConfig {
 
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
 
         // Clave: DESERIALIZADORES AVRO
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
